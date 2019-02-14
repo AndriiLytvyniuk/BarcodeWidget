423ba19528c5b2658b1994dfc3923ebc4f029579
AndriiLytvyniuk <alytvyniuk@mobileiron.com>
2019-02-14 12:20:38 +0200

    extend CodeToImageConverter from AsyncConverter

--- app/src/main/java/alytvyniuk/com/barcodewidget/converters/CodeToImageConverter.kt
+++ app/src/main/java/alytvyniuk/com/barcodewidget/converters/CodeToImageConverter.kt
@@ -1,37 +1,18 @@
 package alytvyniuk.com.barcodewidget.converters
 
+import alytvyniuk.com.barcodewidget.dagger.AsyncConverter
 import alytvyniuk.com.barcodewidget.model.Barcode
 import android.graphics.Bitmap
-import androidx.annotation.VisibleForTesting
-import java.lang.IllegalStateException
+import android.os.Handler
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class CodeToImageConverter @Inject constructor(
-    val barcodeToBitmap: BarcodeToBitmap
-) {
 
-    interface BitmapListener {
-        fun onBitmapConverted(bitmap: Bitmap)
-    }
+class CodeToImageConverter @Inject constructor(handler: Handler,
+                                               val barcodeToBitmap: BarcodeToBitmap)
+    : AsyncConverter<Barcode, Bitmap>(handler) {
 
-    @VisibleForTesting
-    var listener: BitmapListener? = null
-
-    @Synchronized
-    fun convert(barcode: Barcode, bitmapListener: BitmapListener) {
-        if (this.listener != null) {
-            throw IllegalStateException("BarcodeToBitmapConverter supports only one image at time")
-        }
-        this.listener = bitmapListener
-        performConversion(barcode)
-    }
-
-    @VisibleForTesting
-    fun performConversion(barcode: Barcode) {
-        val bitmap = barcodeToBitmap.convert(barcode)
-        listener?.onBitmapConverted(bitmap)
-        listener = null
+    override fun performConversion(from: Barcode) {
+        val result = barcodeToBitmap.convert(from)
+        sendResult(result)
     }
 }
