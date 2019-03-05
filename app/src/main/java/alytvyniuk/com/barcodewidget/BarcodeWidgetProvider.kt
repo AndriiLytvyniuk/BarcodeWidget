8869bd09998a064c78e9465153294569e6333339
AndriiLytvyniuk <alytvyniuk@mobileiron.com>
2019-03-05 17:10:27 +0200

    added handling BOOT_COMPLETED

--- app/src/main/java/alytvyniuk/com/barcodewidget/BarcodeWidgetProvider.kt
+++ app/src/main/java/alytvyniuk/com/barcodewidget/BarcodeWidgetProvider.kt
@@ -5,6 +5,7 @@ import alytvyniuk.com.barcodewidget.db.BarcodeDao
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -18,6 +19,16 @@ class BarcodeWidgetProvider : AppWidgetProvider() {
 
     @Inject lateinit var codeToImageConverter: CodeToImageConverter
     @Inject lateinit var barcodeDao: BarcodeDao
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d(TAG, "onReceive ${Intent.ACTION_BOOT_COMPLETED}")
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val widgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, javaClass))
+            onUpdate(context, AppWidgetManager.getInstance(context), widgetIds)
+        }
+    }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
