e5e70da6c0b660beb1e8ab8e5bd692ba962abcf5
AndriiLytvyniuk <alytvyniuk@mobileiron.com>
2019-03-20 18:06:04 +0200

    recycler view first tests

--- app/src/androidTest/java/alytvyniuk/com/barcodewidget/ListActivityTest.kt
+++ app/src/androidTest/java/alytvyniuk/com/barcodewidget/ListActivityTest.kt
@@ -3,24 +3,38 @@ package alytvyniuk.com.barcodewidget
 import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
 import org.junit.Test
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.ViewAssertion
-import junit.framework.Assert.assertEquals
-
+import org.junit.Assert.assertEquals
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Before
+import testutils.RecyclerViewMatcher
 
 class ListActivityTest {
 
-    @get:Rule
-    val rule = ActivityTestRule<ListActivity>(ListActivity::class.java)
+    @Before
+    fun before() {
+        ActivityScenario.launch(ListActivity::class.java)
+    }
 
     @Test
     fun checkListViewCount() {
         onView(withId(R.id.barcodeListView)).check(RecyclerViewItemCountAssertion(3))
     }
+
+    @Test
+    fun checkListViewItem() {
+        onView(withRecyclerView(R.id.barcodeListView).atPositionOnView(0, R.id.dataTextView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("first")))
+    }
+}
+
+fun withRecyclerView(recyclerViewId: Int): RecyclerViewMatcher {
+    return RecyclerViewMatcher(recyclerViewId)
 }
 
 class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
