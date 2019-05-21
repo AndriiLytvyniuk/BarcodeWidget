aa2c387d8f35519d3e88b6c6f102e2af690070b6
AndriiLytvyniuk <alytvyniuk@mobileiron.com>
2019-05-21 13:46:36 +0300

    test for ViewModel Coroutines

---
+++ app/src/androidTest/java/alytvyniuk/com/barcodewidget/ImageConvertViewModelTest.kt
@@ -0,0 +1,12 @@
+package alytvyniuk.com.barcodewidget
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ImageConvertViewModelTest {
+
+    @Test
+    fun performConversion() {
+    }
+}
